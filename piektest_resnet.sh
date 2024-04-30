4ef43c7c0cb4d24e6297186cb19a5a7004c1e51f
dbreen13 <demibreen@gmail.com>
2024-04-30 21:15:37 +0200

    jdhdhs

--- piektest_resnet.sh
+++ piektest_resnet.sh
@@ -26,6 +26,11 @@ for i in {1..5};do
 done; 
 
 for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-54.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i --cuda='0';
+done; 
+
+for i in {1..5};do
     CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-51.yml";
     python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
 done; 
