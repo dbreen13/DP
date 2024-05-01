d61c19750cde45d0b2158aecab2b93ba503d3c9b
dbreen13 <demibreen@gmail.com>
2024-05-01 21:07:35 +0200

    a

--- piektest_resnet.sh
+++ piektest_resnet.sh
@@ -5,47 +5,6 @@
 #baseline
 
 #experiment1
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-60.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.60.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-54.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.9-54.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.9 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.60.54.51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
-done; 
-
-#experiment 2
-
-for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.2-51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.2 --index=$i --cuda='0';
-done; 
 
 for i in {1..5};do
     CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.30.2-54.51.yml";
@@ -53,6 +12,35 @@ for i in {1..5};do
 done; 
 
 for i in {1..5};do
-    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.2-54.51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.2 --index=$i --cuda='0';
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-60.51.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
 done; 
+
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.51.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
+done; 
+
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-63.54.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
+done; 
+
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-54.60.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
+done; 
+
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.1-51.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
+done; 
+
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.1-51.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0';
+done; 
+
+
+
+
