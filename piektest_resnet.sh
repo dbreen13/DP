256e2195805588ec0825a9593d403949b93248c7
dbreen13 <demibreen@gmail.com>
2024-05-02 12:09:23 +0200

    d

--- piektest_resnet.sh
+++ piektest_resnet.sh
@@ -9,17 +9,17 @@
 
 for i in {1..5};do
     CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-54.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300;
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300 --seed=1;
 done;
 
 for i in {1..5};do
     CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300;
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300 --seed=1;
 done; 
 
 for i in {1..5};do
     CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.1-54.51.yml";
-    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300;
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.1 --index=$i --cuda='0' --batch=300 --seed=1;
 done; 
 
 
