11bb3c6f078543ec0f42a50eabc56f1172b74b1e
dbreen13 <demibreen@gmail.com>
2024-05-05 17:17:37 +0200

    hopelijl

--- alltests.sh
+++ alltests.sh
@@ -10,11 +10,12 @@ for i in {1..5};do
 done; 
 
 #experiments
-for i in {1..5};do
-    for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
-        do for RANK in 1 2 3 4 5 6 7 8 9;
-            CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.$RANK-$LAYER.yml";
-            python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
+
+for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
+    do for RANK in 1 2 3 4 5 6 7 8 9;
+        for i in {1..5};do
+        CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.$RANK-$LAYER.yml";
+        python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
     done;    
 done; 
