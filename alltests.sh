c6c968944d965a7160703ec029a71161a6a50908
dbreen13 <demibreen@gmail.com>
2024-05-05 17:02:52 +0200

    huppa

--- alltests.sh
+++ alltests.sh
@@ -4,14 +4,26 @@
 #resnet18 cifar10
 #baseline
 
+for i in {1..5};do
+    CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/train_baseline.yml";
+    python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --index=$i --epochs=25 --gamma=0 --milestones=None --lr=1e-5 --cuda='0' --seed=1;
+done; 
+
 #experiments
 for i in {1..5};do
     for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
         do for RANK in 1 2 3 4 5 6 7 8 9;
-            do for FACT in cp tucker;
-                CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-$FACT-r0.$RANK-$LAYER.yml";
-                python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0';
-            done; 
+            CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.$RANK-$LAYER.yml";
+            python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
+        done; 
+    done;    
+done; 
+
+for i in {1..5};do
+    for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
+        do for RANK in 1 2 3 4 5 6 7 8 9;
+            CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.$RANK-$LAYER.yml";
+            python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
     done;    
 done; 
@@ -21,7 +33,7 @@ for i in {1..5};do
     for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
         do for RANK in 1 2 3 4 5 6 7 8 9;
             CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.$RANK-$LAYER.yml";
-            python train.py main --config-path "$CONFIG_PATH" --data_workers=4 --rank=0.$RANK --index=$i --cuda='0';
+            python train.py main --config-path "$CONFIG_PATH" --data_workers=4 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
     done;    
 done; 
