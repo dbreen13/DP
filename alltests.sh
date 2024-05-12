bf1f1c4fc686b02cf24f8deca8dc95aa8b4f0032
dbreen13 <demibreen@gmail.com>
2024-05-12 17:12:30 +0200

    LL

--- alltests.sh
+++ alltests.sh
@@ -11,18 +11,18 @@
 
 #experiments
 
-for LAYER in 54 51 47 41 38 35 28 25 22 19 15 6; do
+for LAYER in 63 60 57 54 51 47 41 38 35 28 25 22 19 15 6; do
     for RANK in 1 2 3 4 5 6 7 8 9; do
         for i in {1..5}; do
-           CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-cp-r0.$RANK-$LAYER.yml";
-           python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1 --epochs=23;
+           CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.$RANK-$LAYER.yml";
+        python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
     done;    
 done; 
 
 
 #for i in {1..5};do
-#    for LAYER in 63 60 57 54 51 47 41 38 35 28 22 19 15 6;
+#    for LAYER in 63 60 57 54 51 47 41 38 35 28 25 22 19 15 6;
  #       do for RANK in 1 2 3 4 5 6 7 8 9;
   #          CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.$RANK-$LAYER.yml";
    #         python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1 --epochs=23;
@@ -32,7 +32,7 @@ done;
 
 
 #for i in {1..5};do
-   # for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 22 19 15 12 9 6;
+   # for LAYER in 63 60 57 54 51 47 44 41 38 35 31 28 25 22 19 15 12 9 6;
     #    do for RANK in 1 2 3 4 5 6 7 8 9;
      #       CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.$RANK-$LAYER.yml";
       #      python train.py main --config-path "$CONFIG_PATH" --data_workers=4 --rank=0.$RANK --index=$i --cuda='0' --seed=1 --epochs=23;
