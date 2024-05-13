d010af2308711bbc618de0769d0173ee75bfebea
dbreen13 <demibreen@gmail.com>
2024-05-13 15:25:28 +0200

    again

--- alltests.sh
+++ alltests.sh
@@ -4,18 +4,18 @@
 #resnet18 cifar10
 #baseline
 
-#for i in {1..5};do
- #   CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/train_baseline.yml";
-  #  python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --index=$i --epochs=25 --gamma=0 --milestones=None --lr=1e-5 --cuda='0' --seed=1;
-#done; 
+for i in {1..5};do
+   CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/train_baseline.yml";
+   python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --index=$i --epochs=25 --gamma=0 --milestones=None --lr=1e-5 --cuda='0' --seed=1;
+done; 
 
 #experiments
 
-for LAYER in 63 60 57 54 51 47 41 38 35 28 25 22 19 15 6; do
+for LAYER in 57 54 51 47 41 38 35 28 25 22 19 15 6; do
     for RANK in 1 2 3 4 5 6 7 8 9; do
         for i in {1..5}; do
            CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tucker-r0.$RANK-$LAYER.yml";
-        python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
+           python train.py main --config-path "$CONFIG_PATH" --data_workers=8 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
     done;    
 done; 
