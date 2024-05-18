8fa4c9ff282545e2c5641999908cdd2aca15541d
dbreen13 <demibreen@gmail.com>
2024-05-18 20:23:08 +0200

    da

--- alltests.sh
+++ alltests.sh
@@ -31,9 +31,10 @@
 #done; 
 
 
-for i in {1..5};do
-    for LAYER in 63 60 57 54 51 47 41 38 35 28 25 22 19 15 6;
-        do for RANK in 1 2 3 4 5 6 7 8 9;
+
+for LAYER in 63 60 57 54 51 47 41 38 35 28 25 22 19 15 6;do
+    for RANK in 1 2 3 4 5 6 7 8 9;do
+        for i in {1..5};do
             CONFIG_PATH="/home/dbreen/Documents/tddl/papers/iclr_2023/configs/rn18/cifar10/decompose/dec-tt-r0.$RANK-$LAYER.yml";
             python train.py main --config-path "$CONFIG_PATH" --data_workers=4 --rank=0.$RANK --index=$i --cuda='0' --seed=1;
         done; 
