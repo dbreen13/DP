0cd85426afec42749a52955021e6602ca7149dac
dbreen13 <demibreen@gmail.com>
2024-06-11 15:54:29 +0200

    d

--- toy_problems/Data/generate_input.py
+++ toy_problems/Data/generate_input.py
@@ -12,7 +12,7 @@ import pickle
 mean_CIFAR10 = [0.49139968, 0.48215841, 0.44653091]
 std_CIFAR10 = [0.24703223, 0.24348513, 0.26158784]
 batch=128
-sizes=[[batch,64,4,4], [batch,128,4,4], [batch,256,4,4],[batch,512,4,4],[batch,64,2,2], [batch,64,8,8],[batch,64,16,16]]
+sizes=[[batch,448,4,4], [batch,448,2,2], [batch,448,6,6],[batch,448,8,8]]
 for size in sizes:
     
     tens=np.random.normal(loc=mean_CIFAR10[0], scale=std_CIFAR10[0], size=size)
