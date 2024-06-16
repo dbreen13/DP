485a1f5f64a99dfc9b82e5b13021198389b00332
dbreen13 <57091342+dbreen13@users.noreply.github.com>
2024-06-16 19:51:26 +0200

    GDASGSD
    gags

--- toy_problems/Inference/inferenefin.py
+++ toy_problems/Inference/inferenefin.py
@@ -19,7 +19,7 @@ transform = transforms.Compose([
 testset = datasets.CIFAR10(root='/home/dbreen/Documents/tddl/bigdata/cifar10', train=True, download=False, transform=transform)
 
 batch_size = 128
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # Setup the inference logger
@@ -27,7 +27,7 @@ logging.basicConfig(level=logging.INFO)
 inference_logger = logging.getLogger('Inferencefin')
 
 if not any(isinstance(handler, logging.FileHandler) for handler in inference_logger.handlers):
-    fh = logging.FileHandler('inferencefin.log')
+    fh = logging.FileHandler('janken.log')
     fh.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
@@ -45,7 +45,7 @@ if not any(isinstance(handler, logging.FileHandler) for handler in loading_logge
 
 layers=[63,57,51,47,41,35,28,25,19,6]
 compression = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-methods = ['cp','tucker','tt']
+methods = ['tt']
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -122,7 +122,56 @@ for method in methods:
             # Clear memory after each iteration
             del model
             torch.cuda.empty_cache()
+            
 
+testset = datasets.CIFAR10(root='/home/dbreen/Documents/tddl/bigdata/cifar10', train=True, download=False, transform=transform)
+
+batch_size = 128
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+for method in ['cp','tucker']:
+    for layer in layers:
+        for compr in compression:
+            for runnr in ['runnr1', 'runnr2', 'runnr3', 'runnr4', 'runnr5']:
+                path = f"/media/jkooij/d63a895a-7e13-4bf0-a13d-1a6678dc0e38/dbreen/bigdata/cifar10/logs/rn18/decomposed/fact-{method}-r{compr}-lay[{layer}]-b128/{runnr}/rn18-lr-[{layer}]-{method}-{compr}-dTrue-iNone_bn_128_sgd_l1e-05_g0.0_sFalse/fact_model_final.pth"
+                loading_logger.info(f'Trying to load model from {path}')
+                
+                model = load_model(path)
+                if model is not None:
+                    break
+            else:
+                error_files.append(path)
+                continue  # Skip to the next iteration if no model could be loaded
+            
+            model.to(device)
+            correct = 0
+            total = 0
+
+            with torch.no_grad():
+                for i in [1, 2, 3]:
+                    timers.sleep(60)
+                    now = datetime.now()
+                    sec_wait = 60 - now.second
+                    timers.sleep(sec_wait)
+    
+                    inference_logger.info(f'start-inf-{method}-r{compr}-lay[{layer}]-ind{i}')
+                    for s in range(150):
+                        t = tqdm(testloader, total=int(len(testloader)))
+                        for s, data in enumerate(t):
+                            images, labels = data
+                            images = images.to(device)  # Move input data to the same device as the model
+                            labels = labels.to(device)  # Move labels to the same device as the model
+                            outputs = model(images)  # calculate outputs by running images through the network
+                            _, predicted = torch.max(outputs.data, 1)  # the class with the highest energy is what we choose as prediction
+                            total += labels.size(0)
+                            correct += (predicted == labels).sum().item()
+                    inference_logger.info(f'end-inf-{method}-r{compr}-lay[{layer}]-ind{i}')
+            
+            # Clear memory after each iteration
+            del model
+            torch.cuda.empty_cache()
 # Save the list of error files to a text file
 with open('error_files.txt', 'w') as f:
     for item in error_files:
