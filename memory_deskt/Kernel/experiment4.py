c4c625b03f69c4828c0acf4d17ffd091f8912fb6
dbreen13 <demibreen@gmail.com>
2024-06-30 17:39:20 +0200

    ddddd

--- memory_deskt/Kernel/experiment4.py
+++ memory_deskt/Kernel/experiment4.py
@@ -179,7 +179,7 @@ cnn_dict={"in_channels": in_chan,
           "padding": padding}
 
 compression=[0.1,0.25,0.5,0.75,0.9]
-methods=['tt','tucker','cp']
+methods=['nd']
 decompose=True
 
 #create loop with all values to be determined
