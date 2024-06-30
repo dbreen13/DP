de20bce7b77238f628e16aee474a1d8c82a2e9fe
dbreen13 <57091342+dbreen13@users.noreply.github.com>
2024-06-30 13:00:22 +0200

    fff
    fff

--- toy_problems/Features/experiment3.py
+++ toy_problems/Features/experiment3.py
@@ -177,13 +177,13 @@ cnn_dict={"in_channels": in_chan,
 #already defined params, steady for this type of experiment
 
 
-compression=[0.75,0.9]
+compression=[0.1,0.25,0.5]
 methods=['cp']
 decompose=True
 
 #create loop with all values to be determined
 #cp decomposition
-for feature in [6,8]:
+for feature in [8]:
     img_h=feature
     img_w=img_h
     cnn_dict.update({"img_h": img_h})
@@ -206,46 +206,46 @@ for feature in [6,8]:
                 fact_dict={"decompose":decompose,
                             "factorization": method,
                             "rank" : c}
-                for ind in [1,2]:
+                for ind in [1,2,3]:
                     fact_dict.update({'index':ind})
                     model=run_model(x,cnn_dict,fact_dict)
                 
                 
-#already defined params, steady for this ty]
+# #already defined params, steady for this ty]
 
-compression=[0.1,0.25,0.5,0.75,0.9]
-methods=['tucker','tt', 'nd']
-decompose=True
+# compression=[0.1,0.25,0.5,0.75,0.9]
+# methods=['tucker','tt', 'nd']
+# decompose=True
 
-#create loop with all values to be determined
-#cp decomposition
-for feature in [6,8]:
-    img_h=feature
-    img_w=img_h
-    cnn_dict.update({"img_h": img_h})
-    cnn_dict.update({"img_w": img_w})
+# #create loop with all values to be determined
+# #cp decomposition
+# for feature in [6,8]:
+#     img_h=feature
+#     img_w=img_h
+#     cnn_dict.update({"img_h": img_h})
+#     cnn_dict.update({"img_w": img_w})
     
-    with open(f'/home/dbreen/Documents/DP2/DP/toy_problems/Data/inch{in_chan}-wh{img_h}.pkl','rb') as f:  
-        x = pickle.load(f)
+#     with open(f'/home/dbreen/Documents/DP2/DP/toy_problems/Data/inch{in_chan}-wh{img_h}.pkl','rb') as f:  
+#         x = pickle.load(f)
 
-    x=x.float()
-    x=x.cuda()
+#     x=x.float()
+#     x=x.cuda()
     
-    for method in methods:
-        if method=='nd':
-            for ind in [1,2]:
-                fact_dict={"decompose":False, "factorization":'c', "rank":0}
-                fact_dict.update({'index':ind})
-                model=run_model(x,cnn_dict,fact_dict)
-        else:
-            for c in compression:
-                fact_dict={"decompose":decompose,
-                            "factorization": method,
-                            "rank" : c}
-                for ind in [1,2]:
-                    fact_dict.update({'index':ind})
-                    model=run_model(x,cnn_dict,fact_dict)            
-# #tucker
+#     for method in methods:
+#         if method=='nd':
+#             for ind in [1,2]:
+#                 fact_dict={"decompose":False, "factorization":'c', "rank":0}
+#                 fact_dict.update({'index':ind})
+#                 model=run_model(x,cnn_dict,fact_dict)
+#         else:
+#             for c in compression:
+#                 fact_dict={"decompose":decompose,
+#                             "factorization": method,
+#                             "rank" : c}
+#                 for ind in [1,2]:
+#                     fact_dict.update({'index':ind})
+#                     model=run_model(x,cnn_dict,fact_dict)            
+# # #tucker
 # for in_ch in [16,32,64,128]:
 #     cnn_dict.update({"in_channels": in_ch})
 #     with open(f'/home/dbreen/Documents/tddl/toy_problems/Data/inch{in_ch}-wh{img_h}.pkl','rb') as f:  
