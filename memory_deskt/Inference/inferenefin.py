a61ef5704ba64071fc3d5549930856c1d4ae2d28
dbreen13 <57091342+dbreen13@users.noreply.github.com>
2024-07-04 17:38:40 +0200

    dd
    ddd

---
+++ memory_deskt/Inference/inferenefin.py
@@ -0,0 +1,221 @@
+import torchvision
+import torchvision.datasets as datasets
+import logging
+import torch
+import torchvision.transforms as transforms
+import time as timers
+
+import pandas as pd
+import pickle
+import numpy as np
+from datetime import datetime
+from tqdm import tqdm
+
+mean_CIFAR10 = [0.49139968, 0.48215841, 0.44653091]
+std_CIFAR10 = [0.24703223, 0.24348513, 0.26158784]
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean_CIFAR10, std_CIFAR10),
+])
+
+torch.manual_seed(1)
+
+testset = datasets.CIFAR10(root='/home/dbreen/Documents/tddl/bigdata/cifar10', train=True, download=False, transform=transform)
+
+batch_size = 128
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=4)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Setup the inference logger
+logging.basicConfig(level=logging.INFO)
+inference_logger = logging.getLogger('Inferencefin')
+
+if not any(isinstance(handler, logging.FileHandler) for handler in inference_logger.handlers):
+    fh = logging.FileHandler('janken.log')
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    inference_logger.addHandler(fh)
+
+# Setup the loading logger
+loading_logger = logging.getLogger('Loadingfin')
+
+if not any(isinstance(handler, logging.FileHandler) for handler in loading_logger.handlers):
+    fh_loading = logging.FileHandler('loadingfin.log')
+    fh_loading.setLevel(logging.INFO)
+    formatter_loading = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh_loading.setFormatter(formatter_loading)
+    loading_logger.addHandler(fh_loading)
+
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+error_files = []  # List to store the paths of files that cause errors
+
+def load_model(path):
+    try:
+        with open(path, 'rb') as f:
+            model = torch.load(f, map_location=device)
+        loading_logger.info(f'Successfully loaded model from {path}')
+        return model
+    except Exception as e:
+        loading_logger.error(f'Error loading model from {path}: {e}')
+        return None
+
+#path="/media/jkooij/d63a895a-7e13-4bf0-a13d-1a6678dc0e38/dbreen/bigdata/cifar10/logs/rn18/baselines/baseline-rn18-cifar10-b128/runnr2/rn18_18_dNone_128_adam_l1e-05_g0.0_w0.0_sFalse/cnn_final.pth"   
+#model=torch.load(path)
+#model.eval()
+#with torch.no_grad():
+ #   for i in [1, 2, 3]:
+  #      timers.sleep(60)
+   #     now = datetime.now()
+    #   sec_wait = 60 - now.second
+    #    timers.sleep(sec_wait)
+    
+     #   inference_logger.info(f'start-inf-base-cif-ind{i}')
+      #  for s in range(180):
+       #     with torch.no_grad():
+        #        t = tqdm(testloader, total=int(len(testloader)))
+         #       for s, data in enumerate(t):
+          #          images, labels = data
+           #         images = images.to(device)  # Move input data to the same device as the model
+                    #labels = labels.to(device)  # Move labels to the same device as the model
+            #        outputs = model(images)  # calculate outputs by running images through the network
+        #inference_logger.info(f'end-inf-base-cif-ind{i}')
+
+
+#for method in methods:
+ #   for layer in layers:
+  #      for compr in compression:
+   #         for runnr in ['runnr1', 'runnr2', 'runnr3', 'runnr4', 'runnr5']:
+    #            path = f"/media/jkooij/d63a895a-7e13-4bf0-a13d-1a6678dc0e38/dbreen/bigdata/cifar10/logs/rn18/decomposed/fact-{method}-r{compr}-lay[{layer}]-b128/{runnr}/rn18-lr-[{layer}]-{method}-{compr}-dTrue-iNone_bn_128_sgd_l1e-05_g0.0_sFalse/fact_model_final.pth"
+     #           loading_logger.info(f'Trying to load model from {path}')
+                
+      #          model = load_model(path)
+        #        if model is not None:
+         #           break
+          #  else:
+           #     error_files.append(path)
+            #    continue  # Skip to the next iteration if no model could be loaded
+            
+            #model.to(device)
+            #correct = 0
+            #total = 0
+
+            #with torch.no_grad():
+             #   for i in [1, 2, 3]:
+       #             timers.sleep(60)
+              #      now = datetime.now()
+               #     sec_wait = 60 - now.second
+                #    timers.sleep(sec_wait)
+    
+                 #   inference_logger.info(f'start-inf-{method}-r{compr}-lay[{layer}]-ind{i}')
+                  #  for s in range(180):
+                   #     t = tqdm(testloader, total=int(len(testloader)))
+                    #    for s, data in enumerate(t):
+                     #       images, labels = data
+                      #      images = images.to(device)  # Move input data to the same device as the model
+                        #    labels = labels.to(device)  # Move labels to the same device as the model
+                       #     outputs = model(images)  # calculate outputs by running images through the network
+                         #   _, predicted = torch.max(outputs.data, 1)  # the class with the highest energy is what we choose as prediction
+                          #  total += labels.size(0)
+                           # correct += (predicted == labels).sum().item()
+                    #inference_logger.info(f'end-inf-{method}-r{compr}-lay[{layer}]-ind{i}')
+            
+            # Clear memory after each iteration
+            #del model
+            #torch.cuda.empty_cache()
+            
+
+testset = datasets.CIFAR10(root='/home/dbreen/Documents/tddl/bigdata/cifar10', train=True, download=False, transform=transform)
+
+batch_size = 128
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+path="/media/jkooij/d63a895a-7e13-4bf0-a13d-1a6678dc0e38/dbreen/bigdata/cifar10/logs/rn18/baselines/baseline-rn18-cifar10-b128/runnr2/rn18_18_dNone_128_adam_l1e-05_g0.0_w0.0_sFalse/cnn_final.pth"   
+model=torch.load(path)
+mem_bas_dict={}
+model.eval()
+with torch.no_grad():
+    for i in [1]:
+        inference_logger.info(f'start-inf-base-cif-ind{i}')
+        for s in range(1):
+            with torch.no_grad():
+                t = tqdm(testloader, total=int(len(testloader)))
+                torch.cuda.reset_peak_memory_stats()
+
+                for s, data in enumerate(t):
+                    images, labels = data
+                    images = images.to(device)  # Move input data to the same device as the model
+                    labels = labels.to(device)  # Move labels to the same device as the model
+                    outputs = model(images)  # calculate outputs by running images through the network
+        inference_logger.info(f'end-inf-base-cif-ind{i}')
+        peak_memory=torch.cuda.max_memory_allocated()
+        mem=(peak_memory / (1024 * 1024))
+        key='base-cif'
+        mem_bas_dict[key]={'Mem': np.round(mem,decimals=3)}
+
+df1 = pd.DataFrame.from_dict(mem_bas_dict, orient='index', columns=['Mem'])
+save_path = "mem_bas_rn18.pkl"
+with open(save_path, 'wb') as f:
+    pickle.dump(df1, f)
+
+layers=[63,57,51,47,41,35,28,25,19,6]
+compression = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+methods = ['cp', 'tucker', 'tt']
+mem_dict={}
+for method in methods:
+    for layer in layers:
+        for compr in compression:
+            for runnr in ['runnr1', 'runnr2', 'runnr3', 'runnr4', 'runnr5']:
+                path = f"/media/jkooij/d63a895a-7e13-4bf0-a13d-1a6678dc0e38/dbreen/bigdata/cifar10/logs/rn18/decomposed/fact-{method}-r{compr}-lay[{layer}]-b128/{runnr}/rn18-lr-[{layer}]-{method}-{compr}-dTrue-iNone_bn_128_sgd_l1e-05_g0.0_sFalse/fact_model_final.pth"
+                loading_logger.info(f'Trying to load model from {path}')
+                
+                model = load_model(path)
+                if model is not None:
+                    break
+            else:
+                error_files.append(path)
+                continue  # Skip to the next iteration if no model could be loaded
+            
+            model.to(device)
+            correct = 0
+            total = 0
+
+            with torch.no_grad():
+                for i in [1]:    
+                    inference_logger.info(f'start-inf-{method}-r{compr}-lay[{layer}]-ind{i}')
+                    for s in [1]:
+                        t = tqdm(testloader, total=int(len(testloader)))
+                        torch.cuda.reset_peak_memory_stats()
+                        for s, data in enumerate(t):
+                            images, labels = data
+                            images = images.to(device)  # Move input data to the same device as the model
+                            labels = labels.to(device)  # Move labels to the same device as the model
+                            outputs = model(images)  # calculate outputs by running images through the network
+                            _, predicted = torch.max(outputs.data, 1)  # the class with the highest energy is what we choose as prediction
+                            total += labels.size(0)
+                            correct += (predicted == labels).sum().item()
+                    peak_memory=torch.cuda.max_memory_allocated()
+                    mem=(peak_memory / (1024 * 1024))
+                    inference_logger.info(f'end-inf--ind{i}')
+            
+            # Clear memory after each iteration
+            key=f'{method}-r{compr}-lay[{layer}]'
+            mem_dict[key]={'Mem': np.round(mem,decimals=3)}
+
+df = pd.DataFrame.from_dict(mem_dict, orient='index', columns=['Mem'])
+save_path = "mem_rn18.pkl"
+with open(save_path, 'wb') as f:
+    pickle.dump(df, f)
+
+
+# Save the list of error files to a text file
+with open('error_files.txt', 'w') as f:
+    for item in error_files:
+        f.write(f"{item}\n")
+
+print("List of files that caused errors has been saved to 'error_files.txt'.")
